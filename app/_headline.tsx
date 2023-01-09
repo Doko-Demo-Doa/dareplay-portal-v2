4df17efbb0454a290994b41a8c9091c517bbd9e4
Quan Pham <doraemonfanclub@gmail.com>
2023-01-09 15:46:13 +0700

    Increase strength

--- app/_headline.tsx
+++ app/_headline.tsx
@@ -7,10 +7,10 @@ const HeadlineSection = () => {
   return (
     <Box sx={{ height: "116rem" }}>
       <Parallax
-        blur={10}
+        blur={0}
         bgImage="/images/sample-bg.jpg"
         bgImageAlt="rainy"
-        strength={200}
+        strength={300}
       >
         <Box sx={{ height: "16rem" }}>
           Content goes here. Parallax height grows with content height.
